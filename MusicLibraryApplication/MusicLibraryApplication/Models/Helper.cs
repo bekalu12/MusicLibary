43e63579b94e9a173456f71e4cf98aea937f1a64
Michelle Burroughs <burroughs87@gmail.com>
2022-02-22 05:52:31 -0800

    Added Models

---
+++ MusicLibraryApplication/MusicLibraryApplication/Models/Helper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibraryApplication.Models
+{
+    public static class Helper
+    {
+
+        public static Decades ConvertToDecade(DateTime dateTime)
+        {
+            switch (dateTime.Year)
+            {
+                case int n when n >= 1960 && n < 1970:
+                    Decades decade1 = Decades.Sixies;
+                    return decade1;
+                case int n when n >= 1970 && n < 1980:
+                    Decades decade2 = Decades.Seventies;
+                    return decade2;
+                case int n when n >= 1980 && n < 1990:
+                    Decades decade3 = Decades.Eighties;
+                    return decade3;
+                case int n when n >= 1990 && n < 2000:
+                    Decades decade4 = Decades.Ninties;
+                    return decade4;
+                case int n when n >= 2000 && n < 2010:
+                    Decades decade5 = Decades.Aughts;
+                    return decade5;
+                case int n when n >= 2010 && n < 2020:
+                    Decades decade6 = Decades.Teens;
+                    return decade6;
+                case int n when n >= 2020 && n < 2030:
+                    Decades decade7 = Decades.Twenties;
+                    return decade7;
+                default:
+                    return Decades.Unknown;
+
+            }
+        }
+    }
+}
---
+++ MusicLibraryApplication/MusicLibraryApplication/Models/MusicItem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibraryApplication.Models
+{
+    public enum MusicGenre
+    {
+        Classical,
+        Country,
+        Electronic,
+        Pop,
+        Rap,
+        Rock
+    }
+
+    public enum Decades
+    {
+        Sixies,
+        Seventies,
+        Eighties,
+        Ninties,
+        Aughts,
+        Teens,
+        Twenties,
+        Unknown
+    }
+
+    public class MusicItem
+    {
+        public string SongTitle { get; set; }
+        public string Artist { get; set; }
+        public string AlbumName { get; set; }
+        public string SongLength { get; set; }
+        public MusicGenre Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Label { get; set; }
+        public string ImageFile { get; set; }
+        public string AudioFile { get; set; }
+
+        public MusicItem(string songTitle, string artist, string albumName, string songLength, MusicGenre genre, DateTime releaseDate, string label)
+        {
+            SongTitle = songTitle;
+            Artist = artist;
+            AlbumName = albumName;
+            SongLength = songLength;
+            Genre = genre;
+            ReleaseDate = releaseDate;
+            Label = label;
+
+            ImageFile = $"/Assets/Image/MusicLibrary/{Genre}/{SongTitle}.png";
+            AudioFile = $"/Assets/Audio/{Genre}/{SongTitle}.mp3";
+        }
+
+    }
+}
+
