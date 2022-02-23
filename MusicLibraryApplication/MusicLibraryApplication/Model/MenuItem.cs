6544a7d5eeb0262b35aa70a622bacdebcb5fd26d
Michelle Burroughs <burroughs87@gmail.com>
2022-02-22 16:56:27 -0800

    updated model

--- MusicLibraryApplication/MusicLibraryApplication/Model/MenuItem.cs
+++ MusicLibraryApplication/MusicLibraryApplication/Model/MenuItem.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MusicLibraryApplication.Models
+namespace MusicLibraryApplication.Model
 {
 
     public class MenuItem
--- MusicLibraryApplication/MusicLibraryApplication/Model/MusicManager.cs
+++ MusicLibraryApplication/MusicLibraryApplication/Model/MusicManager.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MusicLibraryApplication.Models
+namespace MusicLibraryApplication.Model
 {
     public static class MusicManager
     {
